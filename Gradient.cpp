cbc8056514e644504408ea705d1e536d6df7d1ba
Jacob Joaquin <jacobjoaquin@gmail.com>
2012-11-04 09:46:11 -0800

    Updated Gradient to use floats and allow user defined colors

--- Gradient.cpp
+++ Gradient.cpp
@@ -1,14 +1,33 @@
 #include "Gradient.h"
 
 Gradient::Gradient() {
+    r0 = 1.0f;
+    g0 = 0.0f;
+    b0 = 0.0f;
+    r1 = 0.0f;
+    g1 = 0.0f;
+    b1 = 1.0f;
 }
 
 void Gradient::fillPixelBuffer(PixelBuffer &pb) {}
 
 void Gradient::putPixel(PixelBuffer &pb, uint16_t c, float x) {
     c *= pb.N_COLORS;
+    float f = 1.0f - x;
+    float r = (r0 * f + r1 * x) * 127.0f;
+    float g = (g0 * f + g1 * x) * 127.0f;
+    float b = (b0 * f + b1 * x) * 127.0f;
 
-    pb.buffer[c] = (uint8_t) (x * 127.0f);
-    pb.buffer[++c] = (uint8_t) (x * 127.0f);
-    pb.buffer[++c] = (uint8_t) (x * 127.0f);
+    pb.buffer[c] = (uint8_t) r;
+    pb.buffer[++c] = (uint8_t) g;
+    pb.buffer[++c] = (uint8_t) b;
+}
+
+void Gradient::setColors(float r0_, float g0_, float b0_, float r1_, float g1_, float b1_) {
+    r0 = r0_;
+    g0 = g0_;
+    b0 = b0_;
+    r1 = r1_;
+    g1 = g1_;
+    b1 = b1_;
 }
--- Gradient.h
+++ Gradient.h
@@ -10,7 +10,9 @@ public:
     Gradient();
     void fillPixelBuffer(PixelBuffer &pb);
     void putPixel(PixelBuffer &pb, uint16_t c, float x);
+    void setColors(float r0_, float g0_, float b0_, float r1_, float g1_, float b1_);
 private:
+    float r0, g0, b0, r1, g1, b1;
 };
 
 #endif
